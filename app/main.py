9f7bdb2ad8f4c36efae774a1f0f280293c31c213
Andreas Calleja <andreascalleja@gmail.com>
2022-12-09 16:05:45 +0100

    Added vote functionality

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from database import engine
-from routers import post, user, auth
+from routers import post, user, auth, vote
 from config import Settings
 import models
 
@@ -12,6 +12,7 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 
 @app.get("/")
